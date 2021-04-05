48f96c86bc2ba6c7d02c225e0cafdfec01a8008e
JAK719 <jkepic19@gmail.com>
2021-04-05 13:33:06 -0400

    initial setup

---
+++ Sailing-Rocks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sailing_Rocks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
