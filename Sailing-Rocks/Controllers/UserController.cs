783438c7e7936986c15313ed6cb8ada7f7f55e47
JE-Xex <77205709+JE-Xex@users.noreply.github.com>
2021-04-06 15:04:42 -0400

    added user controller

---
+++ Sailing-Rocks/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sailing_Rocks.Models;
+using Sailing_Rocks.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sailing_Rocks.Controllers
+{
+    public class UserController : Controller
+    {
+
+        IRepository<User> userRepo;
+
+        public UserController(IRepository<User> userRepo)
+        {
+            this.userRepo = userRepo;
+            
+        }
+
+        public ViewResult Create()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public ViewResult Create(User model)
+        {
+            return View();
+        }
+
+ 
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
