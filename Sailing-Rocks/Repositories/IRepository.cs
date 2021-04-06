6acd8cb7f5688777d6a56272147569ea81b534c5
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-04-06 13:58:58 -0400

    added Irepo and Repo

---
+++ Sailing-Rocks/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using Sailing_Rocks.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sailing_Rocks.Repository
+{
+    public interface IRepository<T> : ISelectList where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Create(T obj);
+        void Delete(T obj);
+        void Update(T obj);
+
+    }
+
+    
+}
