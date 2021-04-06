8178185bff47ba7654fbea9b75b1db2ede4aadee
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-04-06 14:09:08 -0400

    added repositories for each model

---
+++ Sailing-Rocks/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Sailing_Rocks.Models;
+using Sailing_Rocks.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sailing_Rocks.Repositories
+{
+    public class UserRepository : Repository<User>, IRepository<User>
+    {
+        public UserRepository(RockContext db) : base(db)
+        {
+
+        }
+
+    }
+}
