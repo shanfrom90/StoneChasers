d6f44b1d71aaceaeececc808dd350e4223495091
Eric Faler <77645825+emfaler@users.noreply.github.com>
2021-04-19 12:11:52 -0400

    added image upload

---
+++ Sailing-Rocks/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sailing_Rocks.Models
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; }
+
+
+    }
+}
